2b3e4c916f1d3b675bc053937a2f5ad3046f1014
2eeYunHo <mmm01230@gmail.com>
2022-06-22 14:38:48 +0900

    commit

---
+++ springboot-myweb/src/main/java/myweb/MyWebApplication.java
@@ -0,0 +1,34 @@
+package myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MyWebApplication {
+	
+	@Controller
+	public class HelloController {
+		@ResponseBody
+		@GetMapping("/hello")
+		public String hello() {
+				return "hello";
+		}
+	}
+	
+	@RestController
+	public class HelloController2 {
+		@ResponseBody
+		@GetMapping("/hello2")
+		public String hello() {
+				return "hello";
+		}
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebApplication.class, args);
+	}
+}
